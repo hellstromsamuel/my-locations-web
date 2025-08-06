f9183cd05efdfa5642d33bc7ccaa103bc91bf1db
Samuel Hellstrøm <hellstromsamuel@gmail.com>
2025-08-06 10:41:09 +0200

    feat: refactor Header component to accept className prop for flexible styling

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,12 +1,20 @@
+import { cn } from "../../lib/utils";
 import ThemeToggleButtons from "../theme/ThemeToggleButtons";
 
-function Header() {
+interface Props {
+  className?: string;
+}
+
+function Header({ className }: Props) {
   return (
-    <div className="bg-white dark:bg-gray-900">
-      <div className="h-16 px-4 w-full flex justify-between items-center max-w-4xl mx-auto">
-        <h1 className="font-semibold text-xl">My locations</h1>
-        <ThemeToggleButtons />
-      </div>
+    <div
+      className={cn(
+        "h-16 px-4 w-full flex justify-between items-center",
+        className
+      )}
+    >
+      <h1 className="font-semibold text-xl">My locations</h1>
+      <ThemeToggleButtons />
     </div>
   );
 }
