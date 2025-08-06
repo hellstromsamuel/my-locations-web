7c34352377908e7ab776e1599eb359a9e1277c9c
Samuel Hellstrøm <hellstromsamuel@gmail.com>
2025-08-06 10:41:23 +0200

    refactor: replace ThemeProvider with Providers component for improved context management

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ThemeProvider } from "./components/theme/ThemeProvider.tsx";
+import Providers from "./Providers.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
+    <Providers>
       <App />
-    </ThemeProvider>
+    </Providers>
   </StrictMode>
 );
