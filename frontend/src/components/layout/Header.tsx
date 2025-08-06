10f91940827b2b86512ee58c5d295641a85dde65
Samuel Hellstrøm <hellstromsamuel@gmail.com>
2025-08-06 09:07:20 +0200

    refactor: restructure Header component layout for improved styling

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -2,9 +2,11 @@ import ThemeToggleButtons from "../theme/ThemeToggleButtons";
 
 function Header() {
   return (
-    <div className="h-16 px-4 w-full flex justify-between items-center border-b bg-white dark:bg-gray-900">
-      <h1 className="font-semibold text-xl">My locations</h1>
-      <ThemeToggleButtons />
+    <div className="bg-white dark:bg-gray-900">
+      <div className="h-16 px-4 w-full flex justify-between items-center max-w-4xl mx-auto">
+        <h1 className="font-semibold text-xl">My locations</h1>
+        <ThemeToggleButtons />
+      </div>
     </div>
   );
 }
