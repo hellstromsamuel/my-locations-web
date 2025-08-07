ab98fe9f48e4e1a6af3cfe99a20b06d6c712833c
Samuel Hellstrøm <hellstromsamuel@gmail.com>
2025-08-07 15:41:48 +0200

    feat: integrate DrawerDialog and SaveLocationForm in LocationCard for editing locations

--- frontend/src/features/locations/components/LocationCard.tsx
+++ frontend/src/features/locations/components/LocationCard.tsx
@@ -4,8 +4,12 @@ import useDeleteLocation from "../hooks/useDeleteLocation";
 import useSaveLocation from "../hooks/useSaveLocation";
 import type { Location } from "../types/Location";
 import { generateUUID } from "../../../lib/uuid";
+import DrawerDialog from "../../../components/ui/drawer-dialog";
+import SaveLocationForm from "./SaveLocationForm";
+import { useState } from "react";
 
 function LocationCard({ location }: { location: Location }) {
+  const [dialogOpen, setDialogOpen] = useState(false);
   const { mutate: deleteLocation } = useDeleteLocation();
   const { mutate: saveLocation } = useSaveLocation();
 
@@ -19,29 +23,39 @@ function LocationCard({ location }: { location: Location }) {
   }
 
   return (
-    <div className="@container">
-      <div className="flex flex-col gap-4 items-start border p-4 rounded-md @lg:flex-row @lg:items-center @lg:justify-between">
-        <div>
-          <h3 className="font-semibold">{location.name}</h3>
-          <p>{location.description}</p>
-        </div>
+    <>
+      <DrawerDialog
+        title="Edit Location"
+        open={dialogOpen}
+        setOpen={setDialogOpen}
+        children={<SaveLocationForm />}
+      />
 
-        <div className="flex gap-2">
-          <Button variant="outline" onClick={copyLocation}>
-            <Copy />
-          </Button>
-          <Button variant="outline">
-            <Edit />
-          </Button>
-          <Button
-            variant="destructive"
-            onClick={() => deleteLocation(location.id)}
-          >
-            <Trash />
-          </Button>
+      <div className="@container">
+        <div className="flex flex-col gap-4 items-start border p-4 rounded-md @lg:flex-row @lg:items-center @lg:justify-between">
+          <div>
+            <h3 className="font-semibold">{location.name}</h3>
+            <p>{location.description}</p>
+          </div>
+
+          <div className="flex gap-2">
+            <Button variant="outline" onClick={copyLocation}>
+              <Copy />
+            </Button>
+
+            <Button variant="outline" onClick={() => setDialogOpen(true)}>
+              <Edit />
+            </Button>
+            <Button
+              variant="destructive"
+              onClick={() => deleteLocation(location.id)}
+            >
+              <Trash />
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
